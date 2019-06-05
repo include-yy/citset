2d9c98f5d1866a34c541ae917afca555816aa7cd
include-yy <46282552+include-yy@users.noreply.github.com>
2019-06-05 23:27:38 +0800

    Add files via upload

---
+++ Example/primesum.c
@@ -0,0 +1,50 @@
+#include"citset.h"
+#include<stdio.h>
+#define TAX 1000000
+#include<time.h>
+
+void Me(void)
+{
+  static int i = 0;
+  static clock_t me;
+  if(i == 0)
+    {
+      i = 1;
+      me = clock();
+    }
+  else if(i == 1)
+    {
+      printf("\ntime:%u\n",clock() - me);
+      i = 0;
+    }
+}
+int main(void)
+{
+  cit prime = citInit(TAX, ONE);
+  unsigned int i, j;
+  long long int sum = 0;
+  citSet(prime,0,ZERO);
+  citSet(prime,1,ZERO);
+
+  Me();
+  for(i = 0;i < TAX;i++)
+    {
+      if(citGet(prime,i))
+	{
+	  for(j = i + i;j < TAX;j += i)
+	    citSet(prime,j,ZERO);
+	}
+    }
+  Me();
+  for(i = 0;i < TAX;i++)
+    {
+      if(citGet(prime,i))
+	sum += i;
+    }
+  
+  printf("The sum is : %lld",sum);
+
+  citClean(&prime);
+  return 0;
+}
+  
